3fab5d69018b040eeb22bacd00980216a11f764c
Nathan Sollenberger <nsollenberger@gmail.com>
2024-10-19 22:00:03 -0700

    use js extension

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
 
 export default function config(phase) {
 	const isDev = phase === PHASE_DEVELOPMENT_SERVER;
